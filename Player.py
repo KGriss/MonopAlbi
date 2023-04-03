60dc5bffd52b94e459c10d1591465689a004e05d
Griss <37448271+KGriss@users.noreply.github.com>
2023-04-03 09:05:26 +0200

    Ajout de l'interface de Player

---
+++ Player.py
@@ -0,0 +1,20 @@
+class Player():
+    def __init__(self, idPlayer, color, money, isYounth):
+        self.idPlayer = idPlayer
+        self.color = color
+        self.money = money
+        self.isYounth = isYounth
+
+        self.image = None
+        self.progress = 0
+        self.square = 0
+        self.isJailed = False
+
+    def forward(value):
+        pass
+    
+    def gotoSquare(square):
+        pass
+    
+    def addMoney(value):
+        pass
