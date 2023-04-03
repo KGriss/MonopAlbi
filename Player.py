61d08412380b5fa7efb89c5bb8055d0745bdc5b1
TeakkaeT <82292246+TeakkaeT@users.noreply.github.com>
2023-04-03 09:36:34 +0200

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,20 +1,22 @@
 class Player():
+    SQUARE_PER_LINE = 9
+    
     def __init__(self, idPlayer, color, money, isYounth):
         self.idPlayer = idPlayer
         self.color = color
         self.money = money
-        self.isYounth = isYounth
+        #self.isYounth = isYounth
 
         self.image = None
         self.progress = 0
         self.square = 0
         self.isJailed = False
 
-    def forward(value):
+    def forward(self, value):
+        pass       
+    
+    def gotoSquare(self, square):
         pass
     
-    def gotoSquare(square):
-        pass
-    
-    def addMoney(value):
-        pass
+    def addMoney(self, value):
+        self.money += value
